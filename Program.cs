686713a23d57fdc11743bfb93f20245216ba1b8e
Piero Maritano <pieromaritano@gmail.com>
2023-11-28 23:11:27 -0300

    agrego base de datos de memoria

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<VotoDb>(opt => opt.UseInMemoryDatabase("Votos"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
-List<Voto> _votos = new();
-
-app.MapGet("/votos", () => TypedResults.Ok(_votos));
-app.MapPost("/votos", ([FromBody] Voto _nuevoVoto) => {
-    _nuevoVoto.Id = _votos.Count + 1;
-    _votos.Add(_nuevoVoto);
-    return TypedResults.Ok(_nuevoVoto);
+app.MapGet("/votos", (VotoDb _db) => TypedResults.Ok(_db.Votos.ToList()));
+app.MapPost("/votos", ([FromBody] Voto _nuevoVoto, VotoDb _db) => {
+    _db.Votos.Add(_nuevoVoto);
+    _db.SaveChanges();
+    return TypedResults.Ok(_nuevoVoto.Id);
 });
 
 app.Run();
+
+public class VotoDb : DbContext
+{
+    public VotoDb(DbContextOptions<VotoDb> options) : base(options) { }
+
+    public DbSet<Voto> Votos => Set<Voto>();
+}
 
 public class Voto
 {
