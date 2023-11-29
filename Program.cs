2519fc0af27557586fc1decba593593ceaecf7a1
Piero Maritano <pieromaritano@gmail.com>
2023-11-28 22:25:18 -0300

    quito metodos que no vana y achico entidad

--- Program.cs
+++ Program.cs
@@ -1,40 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-List<Candidato> _votos = new();
+List<Voto> _votos = new();
 
 app.MapGet("/votos", () => TypedResults.Ok(_votos));
-app.MapPost("/votos", ([FromBody] Candidato _nuevoCandidato) => {
-    _nuevoCandidato.Id = _votos.Count + 1;
-    _votos.Add(_nuevoCandidato);
-    return TypedResults.Ok(_nuevoCandidato);
-});
-app.MapPut("/votos/{id}", ([FromRoute] int id, [FromBody] Candidato _candidatoActualizado) =>  {
-    foreach (var _candidato in _votos)
-    {
-        if (_candidato.Id == id) 
-        {
-            _candidato.VotanteId = _candidatoActualizado.VotanteId;
-            _candidato.CandidatoId = _candidatoActualizado.CandidatoId;
-            _candidato.Partido = _candidatoActualizado.Partido;
-        }
-    }
-    return TypedResults.Ok(_candidatoActualizado);
-});
-app.MapDelete("/votos/{id}", ([FromRoute] int id) => {
-    _votos = _votos.Where(c => c.Id != id).ToList();
-    return TypedResults.Ok();
+app.MapPost("/votos", ([FromBody] Voto _nuevoVoto) => {
+    _nuevoVoto.Id = _votos.Count + 1;
+    _votos.Add(_nuevoVoto);
+    return TypedResults.Ok(_nuevoVoto);
 });
 
 app.Run();
 
-public class Candidato
+public class Voto
 {
     public int Id { get; set; }
     public int VotanteId { get; set; }
-    public int CandidatoId { get; set; }
-    public bool EsNulo { get; set; }
-    public bool EsImpugnado { get; set; }
+    public int? CandidatoId { get; set; }
 }
